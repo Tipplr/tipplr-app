25f5f5e7be8f6b530e99421fe4f28050f38925d0
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-11-15 17:25:16 -0800

    completes submit dropdown functionality, still need to direct submit input where to go

--- appCollapse.js
+++ appCollapse.js
@@ -19,16 +19,13 @@ for (i = 0; i < coll.length; i += 1) {
 let addFunction = function (event) {
     event.preventDefault()
     const specForm = document.getElementById('inputSpecs');
-    // const newSpecLabel = document.createElement('label');
     const newSpecInput = document.createElement('input');
-    // newSpecLabel.setAttribute('for', 'Specs');
-    // newSpecLabel.innerHTML = 'Specs';
+
     newSpecInput.setAttribute('type', 'text');
     newSpecInput.setAttribute('name', 'specs');
     newSpecInput.setAttribute('id', 'specs');
 
-    // specForm.append(newSpecLabel);
     specForm.append(newSpecInput);
 }
-// addFunction()
+
 add.addEventListener('click', addFunction)
