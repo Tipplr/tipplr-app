4f6ad90faf9a798b3745625582ebad1a6ba25604
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-11-15 17:03:55 -0800

    finishes functionality of recipe.html

--- appCollapse.js
+++ appCollapse.js
@@ -1,24 +1,34 @@
 let coll = document.getElementsByClassName('collapsible');
+let add = document.getElementById('plus');
 let i;
 
-for (i = 0; i < coll.length; i += 1) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle('active');
-        let content = this.nextElementSibling;
-        if (content.style.display === 'block') {
-            content.style.display = 'none';
-        } else {
-            content.style.display = 'block'
-        }
-    });
+let collapsingFunction = function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block'
+    }
 }
 
-// let collapsingFunction = function () {
-//     this.classList.toggle('active');
-//     let content = this.nextElementSibling;
-//     if (content.style.display === 'block') {
-//         content.style.display === 'none';
-//     } else if (content.style.display === 'none') {
-//         content.style.display === 'block'
-//     }
-// }
+for (i = 0; i < coll.length; i += 1) {
+    coll[i].addEventListener("click", collapsingFunction)
+}
+
+let addFunction = function (event) {
+    event.preventDefault()
+    const specForm = document.getElementById('inputSpecs');
+    // const newSpecLabel = document.createElement('label');
+    const newSpecInput = document.createElement('input');
+    // newSpecLabel.setAttribute('for', 'Specs');
+    // newSpecLabel.innerHTML = 'Specs';
+    newSpecInput.setAttribute('type', 'text');
+    newSpecInput.setAttribute('name', 'specs');
+    newSpecInput.setAttribute('id', 'specs');
+
+    // specForm.append(newSpecLabel);
+    specForm.append(newSpecInput);
+}
+// addFunction()
+add.addEventListener('click', addFunction)
