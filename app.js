05ae62768cf7e9301badb18c93358cd6fd07a0f6
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-11-16 14:41:00 -0800

    completes popup functionality for recipe list

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let allTags = []; //holds all tags
 let glasswareIcons = []; // holds filepaths for images
 // "drink" as shorthand for "Cocktail object instance"
 
-const Cocktail = function(name, base, ingr = [], amount = [], glassware, instructions = "", notes = "") { // user-gen or from API, also template for mock dataset
+const Cocktail = function (name, base, ingr = [], amount = [], glassware, instructions = "", notes = "") { // user-gen or from API, also template for mock dataset
     this.name = name;
     this.base = base;
     this.ingr = ingr;
@@ -25,11 +25,11 @@ Cocktail.filtered = [];
 
 // Tied to the user-generated recipe feature
 Cocktail.prototype.specsBuilder = function () { // Takes user form entry as input, pushes to drink specs
-//assemble specs array via loops, perhaps?
-this.specs.push(/*output*/);
+    //assemble specs array via loops, perhaps?
+    this.specs.push(/*output*/);
 }
 
-const Ingredient = function (name, type){
+const Ingredient = function (name, type) {
     this.name = name;
     this.type = type;
 
@@ -41,234 +41,251 @@ Ingredient.userInventory = []; // array of ingredient objects
 
 // Ingredients as inventory object
 // recipe item (Ingred instance with qty/unit values)
-    // Specs instance would be constructed of mult. recipe items
+// Specs instance would be constructed of mult. recipe items
 
 //Functions List
 //Enter website (age verification)
-    //display minimum birth date for 21 years old
-    // "were you born before ___ or after?"
+//display minimum birth date for 21 years old
+// "were you born before ___ or after?"
 //Base website (on load after age check)
-   function getLocalStorage(key) {
-       //for access to user-entered cocktails in userRecipes[] 
-            //combine built-in cocktails with user cocktails into allCocktails []
-        // get userInventory[]
-        if (key === 'cocktails'){
-            console.log('you entered key: cocktails');
-           return JSON.parse(localStorage.getItem('cocktails'));
-        } else if (key === 'ingredients'){
-            console.log('you entered key: ingredients');
-           return JSON.parse(localStorage.getItem('ingredients'));
-        }
-        // STRETCH: get allTags[] 
-    }       
-    function renderThumbnails(array){
-        //render array of allCocktails
-        //render specifically the name and base property of Cocktail object
-        array.forEach((e, index) => {
-            const recipeList = document.getElementById('recipe-list-grid');
-            let renderRecipe = document.createElement('div');
+function getLocalStorage(key) {
+    //for access to user-entered cocktails in userRecipes[]
+    //combine built-in cocktails with user cocktails into allCocktails []
+    // get userInventory[]
+    if (key === 'cocktails') {
+        console.log('you entered key: cocktails');
+        return JSON.parse(localStorage.getItem('cocktails'));
+    } else if (key === 'ingredients') {
+        console.log('you entered key: ingredients');
+        return JSON.parse(localStorage.getItem('ingredients'));
+    }
+    // STRETCH: get allTags[] 
+}
+function renderThumbnails(array) {
+    //render array of allCocktails
+    //render specifically the name and base property of Cocktail object
+    array.forEach((e, index) => {
+        const recipeList = document.getElementById('recipe-list-grid');
+        let renderRecipe = document.createElement('div');
 
-            recipeList.appendChild(renderRecipe);
-            renderRecipe.setAttribute('class', 'recipe-card');
-            renderRecipe.setAttribute('id', `${index}`);
-            renderRecipe.innerHTML = `${e.name} with a base of ${e.base}`;
-
-            console.log(e.ingr);
-            console.log(e.amount);
-        })
+        recipeList.appendChild(renderRecipe);
+        renderRecipe.setAttribute('class', 'recipe-card');
+        renderRecipe.setAttribute('id', `${index}`);
+        renderRecipe.innerHTML = `${e.name} with a base of ${e.base}`;
+    })
 }
 function alphabetFunction() {
     //push cocktail object to allCocktails in alphabetical order
     // add eventlisteners
 }
 // Viewing a recipe:
+const recipeListEventListener = document.getElementById('recipe-list-grid');
+recipeListEventListener.addEventListener('click', renderRecipeCard);
+
 function eventlistener() {
     //to display recipe "card" 
 }
-function renderRecipeCard(array) {
+function renderRecipeCard(event) {
     //change from hidden to shown
     //add event listener for nav left, right, return to list
-    array.forEach((e, index) => {
-        let recipeCard = document.getElementById(`${index}`)
+    let clicked = event.target.id;
 
-        let writeRecipe = document.createElement('div');
-        writeRecipe.setAttribute('class', 'popup-recipe');
-        recipeCard.append(writeRecipe);
+    let recipeCard = document.getElementById('popup');
+    let tempRecipe = Cocktail.all[clicked];
 
-        let icon = document.createElement('div');
-        icon.setAttribute('class', 'icon');
-        writeRecipe.append(icon);
+    if (tempRecipe) {
+
+        recipeCard.classList.remove('hidden');
+        recipeCard.classList.add('recipe-grid');
+
+        // let writeRecipe = document.createElement('div');
+        // recipeCard.append(writeRecipe);
 
         let recipeName = document.createElement('h4');
         recipeName.setAttribute('class', 'rendered-title');
-        writeRecipe.append(recipeName);
-        recipeName.innerHTML = `${e.name}`
+        recipeCard.append(recipeName);
+        recipeName.innerHTML = `${tempRecipe.name}`
 
+        let icon = document.createElement('div');
+        icon.setAttribute('class', 'icon');
+        recipeCard.append(icon);
 
         let ingrs = document.createElement('ul');
-        writeRecipe.append(ingrs);
-        let oneIng = document.createElement('li');
-        ingrs.append(oneIng);
-        for (let i = 0; i < e.ingr.length; i += 1) {
+        ingrs.setAttribute('class', 'ingredients-list');
+        recipeCard.append(ingrs);
+
+        for (let i = 0; i < tempRecipe.ingr.length; i += 1) {
             let oneIng = document.createElement('li');
             ingrs.append(oneIng);
-            oneIng.innerHTML = `${e.ingr[i]} - ${e.amount[i]}`
+            oneIng.innerHTML = `${tempRecipe.ingr[i]} - ${tempRecipe.amount[i]}`
         }
+
         let howToMake = document.createElement('p');
-        writeRecipe.append(howToMake);
-        howToMake.innerHTML = `How to make: ${e.instructions}`
+        howToMake.setAttribute('class', 'how-make');
+        recipeCard.append(howToMake);
+        howToMake.innerHTML = `How to make: ${tempRecipe.instructions}`
 
         let notes = document.createElement('p');
-        writeRecipe.append(notes);
+        notes.setAttribute('class', 'notes');
+        recipeCard.append(notes);
+        notes.innerHTML = `Notes: ${tempRecipe.notes}`
 
-        notes.innerHTML = `Notes: ${e.notes}`
-    })
+        let cancel = document.createElement('button');
+        cancel.setAttribute('class', 'close-popup');
+        cancel.innerHTML = 'X'
+        cancel.addEventListener('click', function () {
+            recipeCard.innerHTML = '';
+            recipeCard.classList.remove('recipe-grid');
+            recipeCard.classList.add('hidden');
+        })
+        recipeCard.append(cancel);
+    }
 }
 
 
 // User story #1: ability to filter the provided cocktail recipe list to view only the recipes possible with current inventory
 
-    // eventlistener on button click
-    //    filters property of base spirt clicked as milestone to story #1
-    function filterDrinksPossible(tolerance) {  
-        // tolerance of 0 if currently possible, tolerance of 1 for one-ingredient-away
-        // filter based on userInventory (see story #5)
-        //outputs filtered array of Cocktails filteredCocktails []
-    }
-    function renderFiltered() {
-        //update the displayed cocktails
-    }
-        
-    
-    function searchBar() { // STRETCH
-        //updates displayFiltered based on cocktail name entered (returns 1 cocktail or more if smart)
-        //call renderFiltered()
-    }
-        
+// eventlistener on button click
+//    filters property of base spirt clicked as milestone to story #1
+function filterDrinksPossible(tolerance) {
+    // tolerance of 0 if currently possible, tolerance of 1 for one-ingredient-away
+    // filter based on userInventory (see story #5)
+    //outputs filtered array of Cocktails filteredCocktails []
+}
+function renderFiltered() {
+    //update the displayed cocktails
+}
+
+
+function searchBar() { // STRETCH
+    //updates displayFiltered based on cocktail name entered (returns 1 cocktail or more if smart)
+    //call renderFiltered()
+}
+
 //User story #2 input ingredients I own, and have the website track it
 
-        // happens when user navs to/refreshes inventory page
-        //get array of ingredient objects stored in the userInventory array
-        //parse JSON
-    
-    function renderIngrList() {
-        //call getLocalStorage('ingredients')
-        //write to DOM a table of ingredients and type from userInventory
-    }
-    function changeInventoryEventListener() {
-        //add an event listener to the add new + button and to the - button next to each ingredient in list
-    }
-    function inventoryHandler() {
-        //if + button is clicked, call showForm()
-        //if - button is clicked call removeIngredient()
-            //this should alert the user to confirm
-    }
-    function showForm() {
-        //makes form fields visible to the user on the inventory page
-        //name, type 
-        //submit
-    }
-    function addIngredient() {
-        //function called on form submit
-        //pushes ingredient into userInventory array
-        //call saveAndRenderInv()
-    }
-    function removeIngredient() {
-        //remove clicked ingredient from the userInventory array
-        //saveAndRenderInv() 
-    } 
-    function saveToLocalStorage(object) {
-        // DONE saves userInventory to local Storage
-        //checks if the ingredient being uploaded is a Cocktail object or an Ingredient Object
-        const objectName = object[0].constructor.name
+// happens when user navs to/refreshes inventory page
+//get array of ingredient objects stored in the userInventory array
+//parse JSON
 
-        if (objectName === 'Cocktail'){
-            localStorage.setItem('cocktails', JSON.stringify(object));
-        } else {
-            localStorage.setItem('ingredients', JSON.stringify(object));
-        }
+function renderIngrList() {
+    //call getLocalStorage('ingredients')
+    //write to DOM a table of ingredients and type from userInventory
+}
+function changeInventoryEventListener() {
+    //add an event listener to the add new + button and to the - button next to each ingredient in list
+}
+function inventoryHandler() {
+    //if + button is clicked, call showForm()
+    //if - button is clicked call removeIngredient()
+    //this should alert the user to confirm
+}
+function showForm() {
+    //makes form fields visible to the user on the inventory page
+    //name, type 
+    //submit
+}
+function addIngredient() {
+    //function called on form submit
+    //pushes ingredient into userInventory array
+    //call saveAndRenderInv()
+}
+function removeIngredient() {
+    //remove clicked ingredient from the userInventory array
+    //saveAndRenderInv() 
+}
+function saveToLocalStorage(object) {
+    // DONE saves userInventory to local Storage
+    //checks if the ingredient being uploaded is a Cocktail object or an Ingredient Object
+    const objectName = object[0].constructor.name
+
+    if (objectName === 'Cocktail') {
+        localStorage.setItem('cocktails', JSON.stringify(object));
+    } else {
+        localStorage.setItem('ingredients', JSON.stringify(object));
     }
-    function clearTable() {
-        //erases rows in table
-    }
-    function saveAndRenderInv() {
-        //call saveToLocalStorage()
-        //call clearTable()
-        //call renderIngrList()
-    }
+}
+function clearTable() {
+    //erases rows in table
+}
+function saveAndRenderInv() {
+    //call saveToLocalStorage()
+    //call clearTable()
+    //call renderIngrList()
+}
 
 
 //User story #3 enter and store my own recipe
-    function recipeEventListner(){
-        //listens for button click to add a recipe
-        const recipeSubmitBtn = document.getElementById('recipeSubmitBtn');
-        recipeSubmitBtn.addEventListener('click', addRecipeHandler);
-    }
+function recipeEventListner() {
+    //listens for button click to add a recipe
+    const recipeSubmitBtn = document.getElementById('recipeSubmitBtn');
+    recipeSubmitBtn.addEventListener('click', addRecipeHandler);
+}
 
-recipeEventListner();
+// recipeEventListner();
 
-    function showNewRecipeForm() {
-        //changes CSS display styling to properly display the recipe card pop-up form
-        // show pop-up card form
-    }
-    function addRecipeHandler(event) {
-        //call showNewRecipeForm()
-        event.preventDefault();
-        newRecipeFormSubmit();
-        showNewRecipeForm();
-    }
+function showNewRecipeForm() {
+    //changes CSS display styling to properly display the recipe card pop-up form
+    // show pop-up card form
+}
+function addRecipeHandler(event) {
+    //call showNewRecipeForm()
+    event.preventDefault();
+    newRecipeFormSubmit();
+    showNewRecipeForm();
+}
 
-    function newRecipeFormSubmit(){
-        //calls new Cocktail construtor
-        //pushes form values into Cocktail constructor
-        //STRETCH: call filterFunction() if applicable new recipe will now show in filtered array
-        //STRETCH: call renderFiltered() re-renders page so if new recipe meets requirements is now displayed on the page
-    }
-    function clearForm(){
-        //clears all form fields
-    }
-    function cancelButton(){
-        //calls clearForm()
-        //changes CSS display styling back so pop-up form is hidden  
-    } 
+function newRecipeFormSubmit() {
+    //calls new Cocktail construtor
+    //pushes form values into Cocktail constructor
+    //STRETCH: call filterFunction() if applicable new recipe will now show in filtered array
+    //STRETCH: call renderFiltered() re-renders page so if new recipe meets requirements is now displayed on the page
+}
+function clearForm() {
+    //clears all form fields
+}
+function cancelButton() {
+    //calls clearForm()
+    //changes CSS display styling back so pop-up form is hidden  
+}
 //User story #4 save drinks my friends like, and filter by them
-    //within the recipe card...
-   function tagEventListener() {
-        //listens for event
-    }
-    function displayField(){
-        //displays friend field and 'add' button
-    }
-    function addTagButton() {
-        //pushes friend name to this.tags array
-        //save allTags to local storage
-    }
-        
+//within the recipe card...
+function tagEventListener() {
+    //listens for event
+}
+function displayField() {
+    //displays friend field and 'add' button
+}
+function addTagButton() {
+    //pushes friend name to this.tags array
+    //save allTags to local storage
+}
+
 // User story #5 which ingredient would most broaden the drinks possible 
-    // Call vvv
-    let almostPossible = filterDrinksPossible(1); //argument of tolerance = 1. Uses userInventory, allCocktails. Outputs array to almostPossible
-            // (# userInventory IN this.specs) === specs.length-1) 
-            // an array of cocktails for which all ingredients except one can be found in userInventory[]
-            // this COULD be the same function as the filter for "is possible", with an argument of 1 passed as the "tolerance". When the function gets used for "is possible", we would pass an argument of 0. 
-    // get highYieldBottle and corresponding newBottleRecipes[]    
-        // from almostPossible[] or at the same time, create array of one-off missing ingredients
-        // .reduce() oneOff[] to get total additional cocktails possible per missing ingredient
-        // .sort() to get highYieldBottle, the highest number of new possible drinks 
-        // use highYieldBottle to .filter() through specs in almostPossible[] to produce newBottleRecipes[]
-    // render highYieldBottle and highYieldRecipes[]
-    
-        
+// Call vvv
+let almostPossible = filterDrinksPossible(1); //argument of tolerance = 1. Uses userInventory, allCocktails. Outputs array to almostPossible
+// (# userInventory IN this.specs) === specs.length-1)
+// an array of cocktails for which all ingredients except one can be found in userInventory[]
+// this COULD be the same function as the filter for "is possible", with an argument of 1 passed as the "tolerance". When the function gets used for "is possible", we would pass an argument of 0. 
+// get highYieldBottle and corresponding newBottleRecipes[]    
+// from almostPossible[] or at the same time, create array of one-off missing ingredients
+// .reduce() oneOff[] to get total additional cocktails possible per missing ingredient
+// .sort() to get highYieldBottle, the highest number of new possible drinks 
+// use highYieldBottle to .filter() through specs in almostPossible[] to produce newBottleRecipes[]
+// render highYieldBottle and highYieldRecipes[]
+
+
 // User story #6 quick access to the app’s cocktail recipes and search feature with minimal clicks/prompts
-    // primarily HTML/CSS driven
+// primarily HTML/CSS driven
 // User story #7 alert notifying me that site users must be at least 21 years old
-    //this is accomplished at the page load
+//this is accomplished at the page load
 
 // const Cocktail = function(name, base, specs = [], glassware, instructions = "", notes = "", tags = [], possible, almostPossible)    
-function generateCocktails(){
+function generateCocktails() {
     const manhattan = new Cocktail("Manhattan", 'Whiskey', ['Whiskey', 'Sweet Vermouth', 'Angostura Bitters'], ['2 oz', '1 oz', '2 dashes'], 'Up');
-    const beesKnees = new Cocktail("Bee's Knees", 'Gin', ['Gin','Lemon', 'Honey Syrup'],['2 oz', '3/4 oz', '3/4 oz'], 'Up');
-    const gimlet = new Cocktail("Gimlet", 'Gin', ['Gin', 'Lime', 'Simple Syrup'],['2 oz', '3/4 oz', '1/2 oz'], 'Up');
-    const mojito = new Cocktail("Mojito", 'Rum', ['Rum', 'Lime', 'Simple Syrup'],['2 oz', '3/4 oz', '1/2 oz'], 'Collins');
+    const beesKnees = new Cocktail("Bee's Knees", 'Gin', ['Gin', 'Lemon', 'Honey Syrup', 'Honey', 'Gin', 'Lemon', 'Honey Syrup', 'Honey', 'Gin', 'Lemon', 'Honey Syrup', 'Honey'], ['2 oz', '3/4 oz', '3/4 oz'], 'Up');
+    const gimlet = new Cocktail("Gimlet", 'Gin', ['Gin', 'Lime', 'Simple Syrup'], ['2 oz', '3/4 oz', '1/2 oz'], 'Up');
+    const mojito = new Cocktail("Mojito", 'Rum', ['Rum', 'Lime', 'Simple Syrup'], ['2 oz', '3/4 oz', '1/2 oz'], 'Collins');
     const rosita = new Cocktail("Rosita", 'Tequila', ['Tequila', 'Sweet Vermouth', 'Dry Vermouth', 'Campari'], ['1 3/4 oz', '1/2 oz', '1/2 oz', '1/2 oz'], 'Rocks');
     const moscowMule = new Cocktail("Moscow Mule", 'Vokda', ['Vodka', 'Lime', 'Ginger Beer '], ['1 1/2 oz', '1/2 oz', 'Top'], 'Mule Mug');
 }
