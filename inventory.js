3d508044f2b1ccc437d374926409f5877121d2b1
erik savage <erik.k.savage@gmail.com>
2021-11-16 21:12:56 -0800

    adds toggleFormDisplay(), updates corresponding event listener

--- inventory.js
+++ inventory.js
@@ -6,60 +6,16 @@ function renderIngrList() {
 }
 function changeInventoryEventListener() {
     //add an event listener to the add new + button and to the - button next to each ingredient in list
-
+    document.getElementById('add-inventory').addEventListener("click", toggleFormDisplay);
 }
 function inventoryHandler() {
     //if + button is clicked, call showForm()
     //if - button is clicked call removeIngredient()
     //this should alert the user to confirm
 }
-function showForm() {
-    let coll = document.getElementsByClassName('collapsible');
-    let add = document.getElementById('plus');
-    let minusBtn = document.getElementById('minus');
-    let i;
-    
-    let collapsingFunction = function () {
-        this.classList.toggle('active');
-        let content = this.nextElementSibling;
-        if (content.style.display === 'block') {
-            content.style.display = 'none';
-        } else {
-            content.style.display = 'block'
-        }
-    }
-    
-    for (i = 0; i < coll.length; i += 1) {
-        coll[i].addEventListener("click", collapsingFunction)
-    }
-    
-    let addFunction = function (event) {
-        event.preventDefault()
-        const specForm = document.getElementById('inputSpecs');
-        const newSpecInput = document.createElement('input');
-    
-        newSpecInput.setAttribute('type', 'text');
-        newSpecInput.setAttribute('name', 'specs');
-        newSpecInput.setAttribute('id', 'specs-new');
-    
-        specForm.append(newSpecInput);
-    }
-    
-    // add.addEventListener('click', addFunction)
-    
-    let minusFunction = function (event) {
-        event.preventDefault();
-        const specForm = document.getElementById('inputSpecs');
-        // will throw error if no new spec fields created
-        const newSpecInput = document.getElementById('specs-new');
-        specForm.removeChild(newSpecInput);
-    }
-    
-    // minusBtn.addEventListener('click', minusFunction);
-   
-    //makes form fields visible to the user on the inventory page
-    //name, type 
-    //submit
+function toggleFormDisplay() {
+    const form = document.querySelector(".inventory-form");
+    form.classList.toggle('hidden');
 }
 
 const form = document.querySelector(".inventory-form");
@@ -82,4 +38,4 @@ function removeIngredient() {
 }
 
 //Function Execution START:
-renderIngrList();
+changeInventoryEventListener()
