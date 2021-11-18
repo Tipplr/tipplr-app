ae80d2d22d4656544b9aaa6b0dbe9515fa491b63
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-11-17 17:28:01 -0800

    sorts recipes into most important to least important, and gets ready to compare it

--- app.js
+++ app.js
@@ -81,116 +81,53 @@ function alphabetize(array) {
         let stringa = a.name.toLowerCase();
         let stringb = b.name.toLowerCase();
 
-        if (stringa < stringb){
+        if (stringa < stringb) {
             return -1;
         }
-        if (stringa > stringb){
+        if (stringa > stringb) {
             return 1;
         }
         return 0;
     });
 }
 
-function loadObjects(){
+function loadObjects() {
     let storedCocktails = getLocalStorage('cocktails');
     let cocktail;
 
     let storedIngredients = getLocalStorage('ingredients');
     let ingredient;
 
-    if (storedCocktails){
+    if (storedCocktails) {
         generateCocktails();
-        for(let i = 0; i < storedCocktails.length; i += 1){
-           cocktail = new Cocktail (storedCocktails[i].name, storedCocktails[i].base, storedCocktails[i].ingr, storedCocktails[i].amount, storedCocktails[i].glassware, storedCocktails[i].instructions, storedCocktails[i].notes);
-        
-           Cocktail.userRecipes.push(cocktail); //pushes this cocktail back into userRecipes array
+        for (let i = 0; i < storedCocktails.length; i += 1) {
+            cocktail = new Cocktail(storedCocktails[i].name, storedCocktails[i].base, storedCocktails[i].ingr, storedCocktails[i].amount, storedCocktails[i].glassware, storedCocktails[i].instructions, storedCocktails[i].notes);
+
+            Cocktail.userRecipes.push(cocktail); //pushes this cocktail back into userRecipes array
         }
     } else {
         generateCocktails();
     }
 
-    if (storedIngredients){
+    if (storedIngredients) {
         Ingredient.all = [];
         Ingredient.basic = [];
         Ingredient.userInventory = [];
         Ingredient.userPlusBasicIngr = [];
         basicIngredients()
-        for(let i = 0; i < storedIngredients.length; i += 1){
+        for (let i = 0; i < storedIngredients.length; i += 1) {
             ingredient = new Ingredient(storedIngredients[i].name, storedIngredients[i].type);
 
             Ingredient.userInventory.push(ingredient);
             Ingredient.userPlusBasicIngr.push(ingredient);
         }
-    } else{
+    } else {
         basicIngredients();
     }
 
     alphabetize(Cocktail.all);
     alphabetize(Ingredient.userInventory);
 }
-
-// function renderRecipeCard(event) {
-//     //change from hidden to shown
-//     //add event listener for nav left, right, return to list
-//     let clicked = event.target.id;
-
-//     let recipeCard = document.getElementById('popup');
-//     let recipeCardbg = document.getElementById('popup-outline');
-//     let antiScroll = document.getElementById('body');
-
-//     let tempRecipe = Cocktail.all[clicked];
-
-//     if (tempRecipe) {
-
-//         antiScroll.classList.add('no-scroll');
-//         recipeCard.classList.remove('hidden');
-//         recipeCard.classList.add('recipe-grid');
-//         recipeCardbg.classList.remove('hidden');
-
-//         let recipeName = document.createElement('h4');
-//         recipeName.setAttribute('class', 'rendered-title');
-//         recipeCard.append(recipeName);
-//         recipeName.innerHTML = `${tempRecipe.name}`
-
-//         let icon = document.createElement('div');
-//         icon.setAttribute('class', 'icon');
-//         recipeCard.append(icon);
-
-//         let ingrs = document.createElement('ul');
-//         ingrs.setAttribute('class', 'ingredients-list');
-//         recipeCard.append(ingrs);
-
-//         for (let i = 0; i < tempRecipe.ingr.length; i += 1) {
-//             let oneIng = document.createElement('li');
-//             ingrs.append(oneIng);
-//             oneIng.innerHTML = `${tempRecipe.ingr[i]} - ${tempRecipe.amount[i]}`
-//         }
-
-//         let howToMake = document.createElement('p');
-//         howToMake.setAttribute('class', 'how-make');
-//         recipeCard.append(howToMake);
-//         howToMake.innerHTML = `How to make: ${tempRecipe.instructions}`
-
-//         let notes = document.createElement('p');
-//         notes.setAttribute('class', 'notes');
-//         recipeCard.append(notes);
-//         notes.innerHTML = `Notes: ${tempRecipe.notes}`
-
-//         let cancel = document.createElement('button');
-//         cancel.setAttribute('class', 'close-popup');
-//         cancel.innerHTML = 'X'
-//         cancel.addEventListener('click', function () {
-//             recipeCard.innerHTML = '';
-//             recipeCard.classList.remove('recipe-grid');
-//             recipeCard.classList.add('hidden');
-//             recipeCardbg.classList.add('hidden');
-//             antiScroll.classList.remove('no-scroll');
-//         })
-//         recipeCard.append(cancel);
-//     }
-// }
-
-
 // User story #1: ability to filter the provided cocktail recipe list to view only the recipes possible with current inventory
 // eventlistener on button click
 
@@ -267,21 +204,20 @@ function addTagButton() {
     //save allTags to local storage
 }
 
-// User story #5 which ingredient would most broaden the drinks possible 
 
-    // Call vvv
-    // let almostPossible = filterDrinks(1); //argument of tolerance = 1. Uses userInventory, allCocktails. Outputs array to almostPossible
-            // (# userInventory IN this.specs) === specs.length-1) 
-            // an array of cocktails for which all ingredients except one can be found in userInventory[]
-            // this COULD be the same function as the filter for "is possible", with an argument of 1 passed as the "tolerance". When the function gets used for "is possible", we would pass an argument of 0. 
-    // get highYieldBottle and corresponding newBottleRecipes[]    
-        // from almostPossible[] or at the same time, create array of one-off missing ingredients
-        // .reduce() oneOff[] to get total additional cocktails possible per missing ingredient
-        // .sort() to get highYieldBottle, the highest number of new possible drinks 
-        // use highYieldBottle to .filter() through specs in almostPossible[] to produce newBottleRecipes[]
-    // render highYieldBottle and highYieldRecipes[]
-    
-        
+// Call vvv
+// let almostPossible = filterDrinks(1); //argument of tolerance = 1. Uses userInventory, allCocktails. Outputs array to almostPossible
+// (# userInventory IN this.specs) === specs.length-1) 
+// an array of cocktails for which all ingredients except one can be found in userInventory[]
+// this COULD be the same function as the filter for "is possible", with an argument of 1 passed as the "tolerance". When the function gets used for "is possible", we would pass an argument of 0. 
+// get highYieldBottle and corresponding newBottleRecipes[]    
+// from almostPossible[] or at the same time, create array of one-off missing ingredients
+// .reduce() oneOff[] to get total additional cocktails possible per missing ingredient
+// .sort() to get highYieldBottle, the highest number of new possible drinks 
+// use highYieldBottle to .filter() through specs in almostPossible[] to produce newBottleRecipes[]
+// render highYieldBottle and highYieldRecipes[]
+
+
 
 // User story #6 quick access to the app’s cocktail recipes and search feature with minimal clicks/prompts
 // primarily HTML/CSS driven
@@ -290,71 +226,71 @@ function addTagButton() {
 
 // const Cocktail = function(name, base, specs = [], glassware, instructions = "", notes = "", tags = [], possible, almostPossible)    
 function generateCocktails() {
-  let builtInRecipeData = [
-    // ["name", "base", ingr[], amt[], "glass", "instr", "notes"],
-    // TEMPLATE to add more:
-    // ["", "", [], [], "", "", ""],
+    let builtInRecipeData = [
+        // ["name", "base", ingr[], amt[], "glass", "instr", "notes"],
+        // TEMPLATE to add more:
+        // ["", "", [], [], "", "", ""],
 
-    ["Manhattan", 'Whiskey', ['Whiskey', 'Sweet Vermouth', 'Angostura Bitters'],
-      ['2 oz', '1 oz', '2 dashes'], 'Up'
-    ],
-    ["Bee's Knees", 'Gin', ['Gin', 'Lemon', 'Honey Syrup'],
-      ['2 oz', '3/4 oz', '3/4 oz'], 'Up'
-    ],
-    ["Gimlet", 'Gin', ['Gin', 'Lime', 'Simple Syrup'],
-      ['2 oz', '3/4 oz', '1/2 oz'], 'Up'
-    ],
-    ["Mojito", 'Rum', ['Rum', 'Lime', 'Simple Syrup'],
-      ['2 oz', '3/4 oz', '1/2 oz'], 'Collins'
-    ],
-    ["Rosita", 'Tequila', ['Tequila', 'Sweet Vermouth', 'Dry Vermouth', 'Campari'],
-      ['1 3/4 oz', '1/2 oz', '1/2 oz', '1/2 oz'], 'Rocks'
-    ],
-    ["Moscow Mule", 'Vodka', ['Vodka', 'Lime', 'Ginger Beer '],
-      ['1 1/2 oz', '1/2 oz', 'Top'], 'Mule Mug'
-    ],
-    ["Cosmopolitan", "Vodka", ["Vodka", "Triple Sec", "Lime", "Cranberry"],
-      [2, .75, .75, .5], "Coupe", "Shake and strain --> chilled glass, lime twist", ""
-    ],
-    ["Corpse Reviver #2", "Gin", ["Gin", "Cocchi Americano", "Triple Sec", "Lemon", "Absinthe"],
-      [.75, .75, .75, .75, "rinse"], "Coupe", "Shake and strain --> chilled glass, no garnish", ""
-    ],
-    ["Gold Rush", "Whiskey", ["Bourbon", "Lemon", "Honey Syrup"],
-      [2, .75, .75], "Rocks", "Short shake and strain--> Rocks glass with ice, lemon wheel", ""
-    ],
-    ["French 75", "Gin", ["Gin", "Lemon", "Simple Syrup", "Sparkling Wine"],
-      [1, .5, .5, "top"], "Flute", "Shake and strain into flute, top with sparkling wine. Spiral-cut lemon twist", ""
-    ],
-    ["Little Italy", "Whiskey", ["Rye", "Cynar", "Sweet Vermouth"],
-      [2, .5, .75], "Nick & Nora", "Stir, strain--> chilled glass, cherry", ""
-    ],
-    ["Mai Tai", "Rum", ["Rum", "Lime", "Triple Sec", "Orgeat", "Demerara Syrup"],
-      [1.75, 1, .5, .25, .25], "Rocks", "Whip shake with one cube, dump into rocks glass and top with crushed or pellet ice. Garnish with mint bouquet, a drizzle of dark rum, and grated nutmeg", ""
-    ],
-    ["Sidecar", "Brandy", ["Cognac", "Dry Curacao", "Lemon", "Demerara Syrup"],
-      [2, .75, .75, "1 barspoon"], "Coupe", "Shake, dbl strain--> Chilled coupe with sugar rim", "TIP: rim half the glass to make it optional"
-    ],
-  ]
+        ["Manhattan", 'Whiskey', ['Whiskey', 'Sweet Vermouth', 'Angostura Bitters'],
+            ['2 oz', '1 oz', '2 dashes'], 'Up'
+        ],
+        ["Bee's Knees", 'Gin', ['Gin', 'Lemon', 'Honey Syrup'],
+            ['2 oz', '3/4 oz', '3/4 oz'], 'Up'
+        ],
+        ["Gimlet", 'Gin', ['Gin', 'Lime', 'Simple Syrup'],
+            ['2 oz', '3/4 oz', '1/2 oz'], 'Up'
+        ],
+        ["Mojito", 'Rum', ['Rum', 'Lime', 'Simple Syrup'],
+            ['2 oz', '3/4 oz', '1/2 oz'], 'Collins'
+        ],
+        ["Rosita", 'Tequila', ['Tequila', 'Sweet Vermouth', 'Dry Vermouth', 'Campari'],
+            ['1 3/4 oz', '1/2 oz', '1/2 oz', '1/2 oz'], 'Rocks'
+        ],
+        ["Moscow Mule", 'Vodka', ['Vodka', 'Lime', 'Ginger Beer '],
+            ['1 1/2 oz', '1/2 oz', 'Top'], 'Mule Mug'
+        ],
+        ["Cosmopolitan", "Vodka", ["Vodka", "Triple Sec", "Lime", "Cranberry"],
+            [2, .75, .75, .5], "Coupe", "Shake and strain --> chilled glass, lime twist", ""
+        ],
+        ["Corpse Reviver #2", "Gin", ["Gin", "Cocchi Americano", "Triple Sec", "Lemon", "Absinthe"],
+            [.75, .75, .75, .75, "rinse"], "Coupe", "Shake and strain --> chilled glass, no garnish", ""
+        ],
+        ["Gold Rush", "Whiskey", ["Bourbon", "Lemon", "Honey Syrup"],
+            [2, .75, .75], "Rocks", "Short shake and strain--> Rocks glass with ice, lemon wheel", ""
+        ],
+        ["French 75", "Gin", ["Gin", "Lemon", "Simple Syrup", "Sparkling Wine"],
+            [1, .5, .5, "top"], "Flute", "Shake and strain into flute, top with sparkling wine. Spiral-cut lemon twist", ""
+        ],
+        ["Little Italy", "Whiskey", ["Rye", "Cynar", "Sweet Vermouth"],
+            [2, .5, .75], "Nick & Nora", "Stir, strain--> chilled glass, cherry", ""
+        ],
+        ["Mai Tai", "Rum", ["Rum", "Lime", "Triple Sec", "Orgeat", "Demerara Syrup"],
+            [1.75, 1, .5, .25, .25], "Rocks", "Whip shake with one cube, dump into rocks glass and top with crushed or pellet ice. Garnish with mint bouquet, a drizzle of dark rum, and grated nutmeg", ""
+        ],
+        ["Sidecar", "Brandy", ["Cognac", "Dry Curacao", "Lemon", "Demerara Syrup"],
+            [2, .75, .75, "1 barspoon"], "Coupe", "Shake, dbl strain--> Chilled coupe with sugar rim", "TIP: rim half the glass to make it optional"
+        ],
+    ]
 
-  builtInRecipeData.forEach(drink => new Cocktail(...drink));
+    builtInRecipeData.forEach(drink => new Cocktail(...drink));
 }
 
 
 function basicIngredients() {
     let basic;
     let basicArray = [
-        ["Rittenhouse Rye","Whiskey"],//TEST remove before deploy
-        ["Roku","Gin"],//TEST remove before deploy
-        ["Bacardi","Rum"],//TEST remove before deploy
-        ["Sweet Vermouth","Vermouth"],//TEST remove before deploy
-        ["Dry Vermouth","Vermouth"],//TEST remove before deploy
-        ["Campari","Liqueur"],//TEST remove before deploy
-        ["Angostura Bitters","Bitters"],//TEST remove before deploy
-        ["Simple Syrup","Basics"],
-        ["Lime","Basics"],
-        ["Lemon","Basics"]
-        ];
-    for (let i = 0; i < basicArray.length; i += 1){
+        ["Rittenhouse Rye", "Whiskey"],//TEST remove before deploy
+        ["Roku", "Gin"],//TEST remove before deploy
+        ["Bacardi", "Rum"],//TEST remove before deploy
+        ["Sweet Vermouth", "Vermouth"],//TEST remove before deploy
+        ["Dry Vermouth", "Vermouth"],//TEST remove before deploy
+        ["Campari", "Liqueur"],//TEST remove before deploy
+        ["Angostura Bitters", "Bitters"],//TEST remove before deploy
+        ["Simple Syrup", "Basics"],
+        ["Lime", "Basics"],
+        ["Lemon", "Basics"]
+    ];
+    for (let i = 0; i < basicArray.length; i += 1) {
         basic = new Ingredient(basicArray[i][0], basicArray[i][1]);
         Ingredient.basic.push(basic);
         Ingredient.userPlusBasicIngr.push(basic);
@@ -362,4 +298,66 @@ function basicIngredients() {
 }
 
 loadObjects();
+
 //const negroni = new Cocktail("Negroni", 'Gin', ['Gin', 'Campari', 'Sweet Vermouth'], ['1 oz', '1 oz', '1 oz'], 'Rocks');
+// User story #5 which ingredient would most broaden the drinks possible 
+
+function mvb() {
+    // console.log(Cocktail.all);
+    let tempArray = [];
+    let numberArray = [];
+    let finalArray = [];
+    // console.log(Ingredient.userPlusBasicIngr);
+    // look through Cocktail.all and find similar NEEDS for drinks
+    // compare the most needed item to the users inventory
+    // this will need to be tucked in a while loop to not stop until it finds something the user is missing
+    console.log(Cocktail.all);
+    Cocktail.all.forEach(e => {
+        e.ingr.forEach(i => {
+            tempArray.push(i);
+        })
+    })
+
+    console.log(tempArray);
+    tempArray.forEach(i => {
+        let tempValueOfI = 0;
+        numberArray.push(tempValueOfI);
+    })
+
+    tempArray.forEach(ingredient => {
+        let tempIngr = ingredient
+        tempArray.forEach((i, index) => {
+            if (tempArray[index] === tempIngr) {
+                numberArray[index] += 1;
+            }
+        })
+    })
+    // console.log(numberArray);
+    tempArray.forEach((e, index) => {
+        finalArray.push([`${numberArray[index]}`, `${tempArray[index]}`])
+    })
+
+    finalArray.sort(function (a, b) {
+        // console.log(a[0]);
+        return b[0] - a[0];
+    });
+
+    console.log(finalArray);
+    console.log(finalArray.indexOf(['5', 'Lemon']));
+    // finalArray.forEach(array => {
+    //     let placeHolderArray = array;
+    //     console.log(placeHolderArray);
+    //     finalArray.forEach((i, index) => {
+    //         // if (finalArray[index] === placeHolderArray) {
+    //         //     finalArray.pop(finalArray[index]);
+    //         // }
+    //     })
+    // })
+    // tempArray.forEach((i, index) => {
+    //     // console.log(i);
+    //     if (tempArray.includes(i)) {
+    //         finalArray[index] += 1;
+    //     }
+    // })
+}
+mvb();
