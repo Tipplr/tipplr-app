fbfe90bfa8de05d46bd63dff61280377879a9284
SpencerTower <spencerjtower1@gmail.com>
2021-11-17 19:50:31 -0800

    removes collapsingFunction()

--- recipe.js
+++ recipe.js
@@ -1,24 +1,9 @@
-// let coll = document.getElementsByClassName('collapsible');
 let add = document.getElementById('plus');
 let minusBtn = document.getElementById('minus');
 let resetBtn = document.getElementById('recipeResetBtn');
 let i;
 let newSpecsIndex = 1;
 let divArray = [];
-
-// let collapsingFunction = function () {
-//     this.classList.toggle('active');
-//     let content = this.nextElementSibling;
-//     if (content.style.display === 'block') {
-//         content.style.display = 'none';
-//     } else {
-//         content.style.display = 'block'
-//     }
-// }
-
-// for (i = 0; i < coll.length; i += 1) {
-//     coll[i].addEventListener("click", collapsingFunction)
-// }
 
 let addFunction = function (event) {
 
