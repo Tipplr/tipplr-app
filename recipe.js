662e4632427d2d192df1feb2c6eab0276d6e0d56
Andrew Enyeart <65784446+aenyeart@users.noreply.github.com>
2021-11-18 13:55:37 -0800

    Revert "Remove ingredient"

--- recipe.js
+++ recipe.js
@@ -1,14 +1,32 @@
+let coll = document.getElementsByClassName('collapsible');
 let add = document.getElementById('plus');
 let minusBtn = document.getElementById('minus');
 let resetBtn = document.getElementById('recipeResetBtn');
 let i;
+// incrementor for new specs id
+// starts at 1 because initial field is 0
+
+let collapsingFunction = function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block'
+    }
+}
+
+for (i = 0; i < coll.length; i += 1) {
+    coll[i].addEventListener("click", collapsingFunction)
+}
+
 let newSpecsIndex = 1;
+
 let divArray = [];
 
 let addFunction = function (event) {
 
-    event.preventDefault();
-
+    event.preventDefault()
     const specForm = document.getElementById('inputSpecs');
     const newSpecDiv = document.createElement('div');
     const newSpecIngrInput = document.createElement('input');
@@ -40,21 +58,33 @@ let addFunction = function (event) {
 
     divArray.push(newSpecDiv);
 
+    console.log('newSpecDiv Id: ' + newSpecDiv);
+    console.log('newSpecsIndex count before increment: ' + newSpecsIndex);
+
     newSpecsIndex += 1;
+
+    console.log('newSpecsIndex count after increment: ' + newSpecsIndex);
 }
+
+add.addEventListener('click', addFunction)
+
+let index = 0;
 
 let minusFunction = function (event) {
 
     event.preventDefault();
     const specForm = document.getElementById('inputSpecs');
+    console.log('specForm.lastChild: ' + specForm.lastChild.id)
+    // will throw error if no new spec fields created
+    console.log('divArray before pop(): ' + divArray);
     if (divArray.length > 0) {
         divArray.pop();
         specForm.lastChild.remove();
         newSpecsIndex -= 1;
     }
+    console.log('newSpecsIndex current count: ' + newSpecsIndex);
 }
 
-add.addEventListener('click', addFunction)
 minusBtn.addEventListener('click', minusFunction);
 
 // function recipeEventListener() {
@@ -69,22 +99,22 @@ function showNewRecipeForm() {
     //changes CSS display styling to properly display the recipe card pop-up form
     // show pop-up card form
 }
-
-// Refactor to switch statement with one listener on recipe form
-const form = document.getElementById('recipe-submit');
-form.addEventListener('submit', addRecipeHandler);
-form.addEventListener('reset', handleResetClick);
-
 function addRecipeHandler(event) {
     //call showNewRecipeForm()
     event.preventDefault();
     newRecipeFormSubmit();
+    showNewRecipeForm();
     clearForm();
 }
+
+const form = document.getElementById('recipe-submit');
+form.addEventListener('submit', addRecipeHandler);
+form.addEventListener('reset', handleResetClick);
 
 function newRecipeFormSubmit() {
     // calls new Cocktail construtor
     // pushes form values into Cocktail constructor
+    // grab field input
 
     const name = form.recipename.value;
     const base = form.base.value;
@@ -95,7 +125,7 @@ function newRecipeFormSubmit() {
     let ingrArray = [];
     let amntArray = [];
     // put ingr and amount into arrays
-    for (let i = 0; i < newSpecsIndex; i += 1) {
+    for (let i = 0; i < newSpecsId; i += 1) {
         const ingr = document.getElementById(`specs-ingr-${i}`).value;
         ingrArray.push(ingr);
         console.log('ingrArray: ' + ingrArray);
@@ -112,7 +142,7 @@ function newRecipeFormSubmit() {
     console.log('newCocktail Instructions: ' + newCocktail.instructions);
 
     Cocktail.userRecipes.push(newCocktail);
-    saveToLocalStorage('Cocktail');
+    saveToLocalStorage(Cocktail.userRecipes);
 
     //STRETCH: call filterFunction() if applicable new recipe will now show in filtered array
     //STRETCH: call renderFiltered() re-renders page so if new recipe meets requirements is now displayed on the page
@@ -134,4 +164,8 @@ function handleResetClick() {
     divArray = [];
     // reset specs index counter
     newSpecsIndex = 1;
+}
+
+function closeFormOnSubmit() {
+
 }
